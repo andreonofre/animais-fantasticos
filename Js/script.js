c41cd2448206e6c06ab9a071702e25ef83197119
André Onofre <alordandre@gmail.com>
2023-07-03 22:00:28 -0300

    modifiquei algumas coisas

---
+++ Js/script.js
@@ -0,0 +1,25 @@
+
+const animais = document.getElementById("animais")
+// console.log(animais)
+
+
+const gridSection = document.getElementsByClassName('grid-section');
+
+// console.log(gridSection[0]);
+
+
+const listas = document.querySelectorAll('ul');
+const titulos = document.querySelectorAll('.titulo');
+
+// Retorna o segundo elemento
+console.log(gridSection[1]);
+
+
+
+const linkCSS = document.querySelector('[href^="https://"]');
+console.log(linkCSS.href)
+
+
+const fotosAnimais = document.querySelectorAll('.animais-lista img');
+
+console.log(fotosAnimais);
